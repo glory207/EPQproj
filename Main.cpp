f7c162e177a65c61fcde5fc08131adaab8657f3d
Glory207 <gloryoti123@gmail.com>
2024-04-09 17:57:09 +0100

    Shader Magic

--- Main.cpp
+++ Main.cpp
@@ -11,6 +11,11 @@ using namespace std;
 #include "VBO.h"
 #include "EBO.h"
 #include "VAO.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 int main()
 {
     glfwInit();
@@ -20,12 +25,12 @@ int main()
     
     GLfloat vertices[] =
     {
-        -0.5f,-0.5f,0.0f,
-        0.0f,-0.5f,0.0f,
-        0.5f,-0.5f,0.0f,
-        0.25f,0.0f,0.0f,
-        0.0f,0.5f,0.0f,
-        -0.25f,0.0f,0.0f,
+        -0.5f,-0.5f,0.0f,   1.0f, 0.5f, 0.2f, 
+        0.0f,-0.5f,0.0f,   0.6f, 0.8f, 0.3f,
+        0.5f,-0.5f,0.0f,   1.0f, 0.5f, 0.2f, 
+        0.25f,0.0f,0.0f,   0.6f, 0.8f, 0.3f,
+        0.0f,0.5f,0.0f,   1.0f, 0.5f, 0.2f, 
+        -0.25f,0.0f,0.0f,   0.6f, 0.8f, 0.3f, 
 
     };
 
@@ -36,7 +41,7 @@ int main()
 
     };
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "win", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "win", NULL, NULL);
 
     if (window == NULL)
     {
@@ -46,15 +51,17 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     gladLoadGL();
-    glViewport(0, 0, 800, 800);
+    glViewport(0, 0, 800, 600);
 
     Shader shaderProgram("default.vert", "default.frag");
     VAO VAO1;
     VAO1.Bind();
     VBO VBO1(vertices, sizeof(vertices));
     EBO EBO1(indices, sizeof(indices));
-    VAO1.LinkVBO(VBO1, 0);
+    VAO1.LinkAttrib(VBO1, 0,3,GL_FLOAT,6 * sizeof(float),(void*)0);
+    VAO1.LinkAttrib(VBO1, 1,3,GL_FLOAT,6 * sizeof(float),(void*)(3 * sizeof(float)));
 
     VAO1.Unbind();
     VBO1.Unbind();
@@ -62,11 +69,14 @@ int main()
     
     glDepthFunc(GL_LESS);
 
+    GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         shaderProgram.Activate();
+        glUniform1f(uniID,pow(sin((float)glfwGetTime()),2));
         VAO1.Bind();
         glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT,0);
         glfwSwapBuffers(window);
@@ -84,6 +94,9 @@ int main()
     return 0;
 };
 
-
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
 
--- default.vert
+++ default.vert
@@ -1,6 +1,10 @@
 #version 330 core
     layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aColor;
+    out vec3 color;
+     uniform float scale;
     void main()
     {
-       gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+       gl_Position = vec4(vec3(aPos.x, aPos.y, aPos.z) + vec3(aPos.x, aPos.y, aPos.z) * scale, 1.0);
+       color = aColor;
     }

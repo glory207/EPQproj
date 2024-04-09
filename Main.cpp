f875f791e14c943b797ded4b0d77e7e8ac94eb82
Glory207 <gloryoti123@gmail.com>
2024-04-09 16:03:17 +0100

    triangle

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,114 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<glm/vec3.hpp>
+#include<glm/mat4x4.hpp>
+using namespace glm;
+using namespace std;
+#include<list>
+
 int main()
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    GLfloat vertices[] =
+    {
+        -0.5f,-0.5f,0.0f,
+        0.5f,-0.5f,0.0f,
+        0.0f,0.5f,0.0f,
+    };
 
-	GLFWwindow* window = glfwCreateWindow(800, 800 ,"win",NULL,NULL);
 
-	if (window == NULL)
-	{
-		std::cout << "RIP BOZO" << std::endl;
-		glfwTerminate();
-		return 1;
-	}
 
-	glfwMakeContextCurrent(window);
-	glViewport(0,0,800,800);
-	while (!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents();
-	}
+    GLFWwindow* window = glfwCreateWindow(800, 800, "win", NULL, NULL);
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    if (window == NULL)
+    {
+        cout << "RIP BOZO" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
 
-	return 0;
-}
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    glViewport(0, 0, 800, 800);
+
+    const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+
+
+    const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n\0";
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+
+    GLuint shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    GLuint VAO,VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(0);
+
+    
+    glDepthFunc(GL_LESS);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+
+    return 0;
+};
+
+
+
+
